29253dbff012044aa0c9478f20f6c0a2aa897717
Tayná Ramos <tayna.ramos@ifood.com.br>
2021-09-27 22:16:38 -0300

    footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  background-color: #f0f0f0;
+  position: absolute;
+  bottom: 0;
+`;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import { Footer } from "./footer";
+
+function index() {
+  return (
+    <Footer>
+      <h5>Realização</h5>
+      <div></div>
+      <section>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </section>
+      <span>
+        2021 © Laboratório de Inovação Tecnológica em Saúde - UFRN. Todos os
+        direitos reservados.
+      </span>
+      <span>j2hv34j32h4v</span>
+    </Footer>
+  );
+}
+
+export default index;
