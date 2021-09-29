91536fa214aeecfee60fe5399d51db5155756a56
Tayná Ramos <tayna.ramos@ifood.com.br>
2021-09-28 22:17:08 -0300

    adição de breadcrumb

---
+++ src/components/Header/header.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  h1 {
+    font-weight: 800;
+    font-size: 1.5rem;
+  }
+  .header-top {
+    padding: 10px 50px;
+    height: 85px;
+    display: flex;
+    align-items: center;
+  }
+
+  .header-bottom {
+    padding: 30px 50px;
+    height: auto;
+  }
+
+  .header-bottom-text {
+    margin-top: 20px;
+  }
+`;
---
+++ src/components/Header/index.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Header } from "./header";
+import {
+  Flex,
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+
+function index() {
+  const router = useRouter();
+  return (
+    <Header>
+      <Box className="header-top" bg="#fff" w="100%" color="#00B3F3">
+        <h1>RN + VACINA</h1>
+      </Box>
+      <Box
+        className="header-bottom"
+        bg="#00B3F3"
+        w="100%"
+        color="#fff"
+        h="15vh"
+      >
+        <Flex direction="column">
+          <div>
+            <Breadcrumb
+              spacing="8px"
+              separator={<ChevronRightIcon color="#fff" />}
+            >
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/inicio">
+                  <Box
+                    as="button"
+                    py={1}
+                    px={3}
+                    borderRadius="3"
+                    bg={router.pathname == "/inicio" ? "#00B3F3" : "#fff"}
+                    color={router.pathname == "/inicio" ? "#fff" : "#00B3F3"}
+                  >
+                    Home
+                  </Box>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="#">
+                  {" "}
+                  <Box
+                    as="button"
+                    py={1}
+                    px={3}
+                    borderRadius="3"
+                    bg={router.pathname == "/" ? "#00B3F3" : "#fff"}
+                    color={router.pathname == "/" ? "#fff" : "#00B3F3"}
+                  >
+                    Cidadão
+                  </Box>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </div>
+          <div className="header-bottom-text">
+            <h1>Bem-vindo ao RN + Vacina Cidadão,</h1>
+            <span>
+              Acompanhe os detalhes de suas vacinas e saiba quando tomar as
+              próximas doses.
+            </span>
+          </div>
+        </Flex>
+      </Box>
+    </Header>
+  );
+}
+
+export default index;
