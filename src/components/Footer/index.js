4a5af097c8eed85702688dd3a4a8e8d323ce05c6
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-10-13 09:22:06 -0300

    refactor: Footer usando componentes do chakra e responsivo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,41 +1,85 @@
 import { ContainerFooter } from "./style";
 import Image from "next/image";
-
-// import sus from "src/assets/images/sus.svg";
-// import lais from "src/assets/images/lais.svg";
-// import ufrn from "src/assets/images/ufrn.svg";
-// import rn from "src/assets/images/rn.svg";
-// import navi from "src/assets/images/navi.png";
-// import ifrn from "src/assets/images/ifrn.png";
-// import digti from "src/assets/images/digti.png";
+import { Flex, Box } from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/react";
 
 export const Footer = () => {
+  const direction = useBreakpointValue({ base: "column", md: "row" });
   return (
     <ContainerFooter>
       <h5>Realização</h5>
       <div className="footer-separator"></div>
-      <section className="footer-imgs">
-        {/* <span className="footer-img">
-          <Image unsized src={sus} alt="" />
-        </span>
-        <span className="footer-img">
-          <Image unsized src={rn} alt="" />
-        </span>
-        <span className="footer-img">
-          <Image unsized src={ufrn} alt="" />
-        </span>
-        <Image unsized src={lais} alt="" />
-        <span className="footer-img">
-          <Image unsized src={navi} alt="" />
-        </span>
-        <span className="footer-img">
-          <Image unsized src={ifrn} alt="" />
-        </span>
-        <span className="footer-img">
-          <Image unsized src={digti} alt="" />
-        </span> */}
-      </section>
-      <span>
+      <Flex justifyContent="center" alignItems="center">
+        <Box
+          maxW="md"
+          display="flex"
+          flexDirection={direction}
+          justifyContent="center"
+          style={{ gap: 20 }}
+        >
+          <Image
+            src="/assets/images/sus.svg"
+            layout="fixed"
+            width={150}
+            height={40}
+            alt=""
+          />
+          <Image
+            src="/assets/images/gov.svg"
+            layout="fixed"
+            width={190}
+            height={50}
+            alt=""
+          />
+          <Image
+            src="/assets/images/ufrn.svg"
+            layout="fixed"
+            width={210}
+            height={50}
+            alt=""
+          />
+          <Image
+            src="/assets/images/lais.svg"
+            layout="fixed"
+            width={200}
+            height={40}
+            alt=""
+          />
+        </Box>
+      </Flex>
+      <Flex justifyContent="center" alignItems="center" my={25} mb={35}>
+        <Box
+          maxW="md"
+          display="flex"
+          justifyContent="center"
+          alignItems="end"
+          flexDirection={direction}
+          style={{ gap: 20 }}
+        >
+          <Image
+            src="/assets/images/navi.png"
+            layout="fixed"
+            width={150}
+            height={40}
+            alt=""
+          />
+          <Image
+            src="/assets/images/ifrn.png"
+            layout="fixed"
+            width={160}
+            height={42}
+            alt=""
+          />
+          <Image
+            src="/assets/images/digti.png"
+            layout="fixed"
+            width={121}
+            height={30}
+            alt=""
+          />
+        </Box>
+      </Flex>
+      <span style={{ marginTop: 40 }}>
         2021 © Laboratório de Inovação Tecnológica em Saúde - UFRN. Todos os
         direitos reservados.
       </span>
