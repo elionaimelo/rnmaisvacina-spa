78ef11442a5214cd639425279618e12f613dc249
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-10-13 10:44:37 -0300

    fix: :bug: Correção do input

--- src/components/FormLogin/index.js
+++ src/components/FormLogin/index.js
@@ -13,11 +13,19 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { Formik, Field, Form } from "formik";
-import FieldCustom from "../Form/FieldCustom";
+import { StylishInput } from "./style";
 
 function FormLogin() {
   const [isDisabled, setIsDisabled] = useState(true);
 
+  function cpfMask(value) {
+    return value
+      .replace(/\D/g, "") // substitui qualquer caracter que nao seja numero por nada
+      .replace(/(\d{3})(\d)/, "$1.$2") // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1"); // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
+  }
   function validateData(values) {
     const errors = {};
     if (!values.cpf) {
@@ -68,40 +76,42 @@ function FormLogin() {
             handleBlur,
           }) => (
             <Form className="agendamento-form">
-              <FieldCustom
-                label="CPF"
-                name="cpf"
-                values={values}
-                errors={errors}
-                touched={touched}
-                handleChange={handleChange}
-                isSubmitting={isSubmitting}
-                setFieldValue={setFieldValue}
-                handleBlur={handleBlur}
-              />
-              <FieldCustom
-                label="Senha"
-                name="password"
-                values={values}
-                errors={errors}
-                touched={touched}
-                handleChange={handleChange}
-                isSubmitting={isSubmitting}
-                setFieldValue={setFieldValue}
-                handleBlur={handleBlur}
-              />
-              {/* <Field name="password">
+              <Field name="cpf">
+                {({ field, form }) => (
+                  <FormControl isInvalid={errors.cpf && touched.cpf}>
+                    <FormLabel htmlFor="cpf">CPF</FormLabel>
+                    <StylishInput
+                      {...field}
+                      id="cpf"
+                      maxLength="14"
+                      value={values.cpf}
+                      onChange={(e) => {
+                        const formatted = cpfMask(values.cpf);
+                        setFieldValue("cpf", formatted);
+                        handleChange(e);
+                      }}
+                      onBlur={(e) => {
+                        const formatted = cpfMask(values.cpf);
+                        setFieldValue("cpf", formatted);
+                        handleBlur(e);
+                      }}
+                    />
+                    <FormErrorMessage>{errors.cpf}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="password">
                 {({ field, form }) => (
                   <FormControl
                     isInvalid={errors.password && touched.password}
                     mt="3"
                   >
                     <FormLabel htmlFor="password">SENHA</FormLabel>
-                    <Input {...field} id="password" type="password" />
+                    <StylishInput {...field} id="password" type="password" />
                     <FormErrorMessage>{errors.password}</FormErrorMessage>
                   </FormControl>
                 )}
-              </Field> */}
+              </Field>
               <Flex flexDirection="column" mt="2">
                 <Link href="/recuperar_senha">
                   <a className="forgot-password">Esqueci minha senha</a>
