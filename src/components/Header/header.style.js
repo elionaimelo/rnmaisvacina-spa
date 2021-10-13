64f7987d4e486a3e287a1aff444a5023a3e818e4
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-10-13 10:21:00 -0300

    feat: :sparkles: Input genérico
    Input genérico para facilitar a estilização

--- src/components/Header/header.style.js
+++ src/components/Header/header.style.js
@@ -1,28 +1,6 @@
 import { Flex } from "@chakra-ui/react";
 import styled from "styled-components";
 
-// export const Container = styled.header`
-//   h1 {
-//     font-weight: 800;
-//     font-size: 1.5rem;
-//   }
-//   .header-top {
-//     padding: 10px 50px;
-//     height: 85px;
-//     display: flex;
-//     align-items: center;
-//   }
-
-//   .header-bottom {
-//     padding: 30px 50px;
-//     height: auto;
-//   }
-
-//   .header-bottom-text {
-//     margin-top: 20px;
-//   }
-// `;
-
 export const FlexCustom = styled(Flex)`
   height: 100%;
 `;
