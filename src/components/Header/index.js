38c02610ede623fdef702323941da49ad12c934c
Tayná Ramos <tayna.ramos@ifood.com.br>
2021-10-14 22:23:01 -0300

    header para usuario autenticado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,7 +13,7 @@ import { ChevronRightIcon } from "@chakra-ui/icons";
 import { FlexCustom, Title } from "./header.style";
 // import logo from "src/assets/images/logo.svg";
 
-export const Header = () => {
+export const Header = ({ auth }) => {
   const router = useRouter();
 
   const style = {
@@ -38,57 +38,41 @@ export const Header = () => {
           </Link>
         </Container>
       </Box>
-      <Box
-        className="header-bottom"
-        bg="#00B3F3"
-        w="100%"
-        color="#fff"
-        h="220px"
-      >
-        <Container maxW="container.xl" h="100%">
-          <FlexCustom direction="column" justifyContent="center">
-            <div>
-              <Breadcrumb
-                spacing="8px"
-                mb={6}
-                separator={<ChevronRightIcon boxSize={5} color="#fff" />}
-              >
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/inicio">
-                    <Box
-                      as="button"
-                      py={1}
-                      px={3}
-                      borderRadius="3"
-                      bg="#fff"
-                      color="#00B3F3"
-                    >
-                      Início
-                    </Box>
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-
-                <BreadcrumbItem
-                  isCurrentPage={router.pathname == "/" ? true : false}
+      {!auth ? (
+        <Box
+          className="header-bottom"
+          bg="#00B3F3"
+          w="100%"
+          color="#fff"
+          h="220px"
+        >
+          <Container maxW="container.xl" h="100%">
+            <FlexCustom direction="column" justifyContent="center">
+              <div>
+                <Breadcrumb
+                  spacing="8px"
+                  mb={6}
+                  separator={<ChevronRightIcon boxSize={5} color="#fff" />}
                 >
-                  <BreadcrumbLink href="/">
-                    {" "}
-                    <Box
-                      as="button"
-                      py={1}
-                      px={1}
-                      borderRadius="3"
-                      bg="#00B3F3"
-                      color="#fff"
-                    >
-                      Cidadão
-                    </Box>
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/inicio">
+                      <Box
+                        as="button"
+                        py={1}
+                        px={3}
+                        borderRadius="3"
+                        bg="#fff"
+                        color="#00B3F3"
+                      >
+                        Início
+                      </Box>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
 
-                {style.color && (
-                  <BreadcrumbItem isCurrentPage>
-                    <BreadcrumbLink href="/recuperar_senha">
+                  <BreadcrumbItem
+                    isCurrentPage={router.pathname == "/" ? true : false}
+                  >
+                    <BreadcrumbLink href="/">
                       {" "}
                       <Box
                         as="button"
@@ -98,23 +82,99 @@ export const Header = () => {
                         bg="#00B3F3"
                         color="#fff"
                       >
-                        Redefinição de senha
+                        Cidadão
                       </Box>
                     </BreadcrumbLink>
                   </BreadcrumbItem>
-                )}
-              </Breadcrumb>
-            </div>
-            <div className="header-bottom-text">
-              <Title>Bem-vindo ao RN + Vacina Cidadão,</Title>
-              <span>
-                Acompanhe os detalhes de suas vacinas e saiba quando tomar as
-                próximas doses.
-              </span>
-            </div>
-          </FlexCustom>
-        </Container>
-      </Box>
+
+                  {style.color && (
+                    <BreadcrumbItem isCurrentPage>
+                      <BreadcrumbLink href="/recuperar_senha">
+                        {" "}
+                        <Box
+                          as="button"
+                          py={1}
+                          px={1}
+                          borderRadius="3"
+                          bg="#00B3F3"
+                          color="#fff"
+                        >
+                          Redefinição de senha
+                        </Box>
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                  )}
+                </Breadcrumb>
+              </div>
+              <div className="header-bottom-text">
+                <Title>Bem-vindo ao RN + Vacina Cidadão,</Title>
+                <span>
+                  Acompanhe os detalhes de suas vacinas e saiba quando tomar as
+                  próximas doses.
+                </span>
+              </div>
+            </FlexCustom>
+          </Container>
+        </Box>
+      ) : (
+        <Box
+          className="header-bottom"
+          bg="#00B3F3"
+          w="100%"
+          color="#fff"
+          h="220px"
+        >
+          <Container maxW="container.xl" h="100%">
+            <FlexCustom direction="column" justifyContent="center">
+              <div>
+                <Breadcrumb
+                  spacing="8px"
+                  mb={6}
+                  separator={<ChevronRightIcon boxSize={5} color="#fff" />}
+                >
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/inicio">
+                      <Box
+                        as="button"
+                        py={1}
+                        px={3}
+                        borderRadius="3"
+                        bg="#fff"
+                        color="#00B3F3"
+                      >
+                        Início
+                      </Box>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+
+                  <BreadcrumbItem
+                    isCurrentPage={
+                      router.pathname == "/agendamento" ? true : false
+                    }
+                  >
+                    <BreadcrumbLink href="/agendamento">
+                      {" "}
+                      <Box
+                        as="button"
+                        py={1}
+                        px={1}
+                        borderRadius="3"
+                        bg="#00B3F3"
+                        color="#fff"
+                      >
+                        Cartão Digital de Vacinação
+                      </Box>
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </Breadcrumb>
+              </div>
+              <div className="header-bottom-text">
+                <Title>Cartão Digital de Vacinação</Title>
+              </div>
+            </FlexCustom>
+          </Container>
+        </Box>
+      )}
     </>
   );
 };
