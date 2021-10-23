df037c289bee89b4dbb102d8b36c66dc73ec1e64
Tayná Ramos <tayna.ramos@ifood.com.br>
2021-10-23 18:36:00 -0300

    removendo codigos comentados

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,14 @@ import GlobalStyle from "src/styles/globalStyles";
 import theme from "src/styles/theme";
 import TemplateWrapper from "src/Templates/TemplateWrapper";
 
-// import { Header, Footer } from "src/components";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <ChakraProvider>
         <GlobalStyle />
-        {/* <Flex direction="column" justify="space-between"> */}
-        {/* <Header auth /> */}
         <TemplateWrapper {...pageProps}>
           <Component {...pageProps} />
         </TemplateWrapper>
-        {/* <Footer /> */}
-        {/* </Flex> */}
       </ChakraProvider>
     </ThemeProvider>
   );
