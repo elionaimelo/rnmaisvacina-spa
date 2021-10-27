329935a8ca83ec2acaa5c8ef332e6fa12371ef0b
Tayná Ramos <tayna.ramos@ifood.com.br>
2021-10-27 11:55:58 -0300

    criaçao de form de dependentes

---
+++ pages/cidadao/painel/adicionar_agendamento.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Container } from "@chakra-ui/react";
+
+export default function AdicionarAgendamento() {
+  return (
+    <>
+      <Head>
+        <title>Adicionar Agendamento</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Container maxW="container.xl">oi</Container>
+    </>
+  );
+}
